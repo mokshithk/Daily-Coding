23da74a6a5fdcfc34908e95874bd755aab22693a
mokshithk <mokshithkulal78@gmail.com>
2026-09-23 20:10:38 +0530

    array program

--- array5.py
+++ array5.py
@@ -1,13 +1,10 @@
 arr = list(map(int, input("Enter array elements: ").split()))
-
 largest = arr[0]
 second_largest = arr[0]
-
 for i in arr:
     if i > largest:
         second_largest = largest
         largest = i
     elif i > second_largest and i != largest:
         second_largest = i
-
 print("Second largest element:", second_largest)
