e20e4dcd40b6370f6892d2185cf2769ffc62a4c9
mokshithk <mokshithkulal78@gmail.com>
2026-09-24 20:31:32 +0530

    linear search

--- linearSearch.py
+++ linearSearch.py
@@ -1,13 +1,10 @@
 arr = list(map(int, input("Enter array elements: ").split()))
 key = int(input("Enter element to search: "))
-
 found = False
-
 for i in range(len(arr)):
     if arr[i] == key:
         print("Element found at index:", i)
         found = True
         break
-
 if not found:
     print("Element not found")
